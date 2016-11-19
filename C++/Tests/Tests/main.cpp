ede8a5397a12cd2186cb085879976d92b6a70ac8
lucien <lcpingping@gmail.com>
2016-11-19 01:18:44 -0800

    manual function
    manual function

--- C++/Tests/Tests/main.cpp
+++ C++/Tests/Tests/main.cpp
@@ -3,55 +3,60 @@
 #include <functional>
 #include <vector>
 #include <sstream>
+#include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
-struct Symbol
-{
-    char name[10];
-    bool operator< (const Symbol& r) const
-    {
-        if (strcmp(name, r.name) < 0)
-            return true;
-        else
-            return false;
+class Solution {
+public:
+    int wordsTyping(vector<string>& sentence, int rows, int cols) {
+        int size = (int)sentence.size();
+        if (size == 0) return 0;
+        
+        // Calculate the complete len's length
+        int len = 0;
+        for (int i=0; i<size; ++i) {
+            len += (int)sentence[i].size() + 1;
+        }
+        
+        int ans = 0; int index = 0;
+        unordered_map<int, pair<int, int>> umap; // first - this string' index, second - <next row's beginning string's index and how many passes for this row>
+        while (rows--) {
+            if (umap.count(index)) {
+                ans += umap[index].second;
+                index = umap[index].first;
+            } else {
+                int maxCols = cols;
+                int i = index;
+                int pass = 0;
+                if ((maxCols-len)/len) {
+                    pass = (maxCols-len)/len;
+                    maxCols -= pass*len;
+                    ans += pass;
+                }
+                while (true) {
+                    if (maxCols >= (int)sentence[i].size()) {
+                        maxCols = maxCols - (int)sentence[i].size() - 1;
+                        ++i;
+                        if (i == size) {
+                            i = 0; ++pass; ++ans;
+                        }
+                    } else break;
+                }
+                umap[index].first = i; umap[index].second = pass;
+                index = i;
+            }
+        }
+        return ans;
     }
-    
 };
-
-vector<string> split (string s, char delimiter) {
-    string temp;
-    stringstream ss(s);
-    vector<string> res;
-    while (getline(ss, temp, '\n')) res.push_back(temp);
-    return res;
-}
-
-
-
-set<Symbol> gSet;
-
 
 int main()
 {
-    string s = "aefawf\nwefawfwa    eawf  awef e\nawefeawfawf\neeeee\nhah  ah  e";
-    vector<string> res = split(s, '\n');
-    for (auto i : res) cout << i <<endl;
+    Solution a;
     
-    cout << endl;
-    
-    Symbol s1 = { "hi" };
-    Symbol s2 = { "girls" };
-    Symbol s3 = { "aaa" };
-    
-    gSet.insert(s1);
-    gSet.insert(s2);
-    gSet.insert(s1);
-    gSet.insert(s2);
-    gSet.insert(s3);
-    
-    for (auto i : gSet) {
-        cout << i.name <<endl;
-    }
+    vector<string> sentence = {"h"};
+    cout << a.wordsTyping(sentence, 2, 3) << endl;
     
     return 0;
 }
